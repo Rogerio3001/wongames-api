1e682fb68c630cc0aa693a72ab6cfde9b0df0f8e
rogerio3001 <60037300+rogerio3001@users.noreply.github.com>
2020-11-17 19:40:49 -0300

    chore: first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1d62e5ba69ba74857e71767647623cf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '385e2f27-7f3d-46be-b3ab-d580e6a96328'
+};
